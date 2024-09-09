5375cbdf0bd1ce1e9451b32235716b2303679dfa
sdomi <ja@sdomi.pl>
2024-09-09 19:39:06 +0200

    Kernel/Net: move internet_checksum to a more general file

--- Kernel/Net/IP/IP.h
+++ Kernel/Net/IP/IP.h
@@ -21,4 +21,20 @@ enum class TransportProtocol : u8 {
     ICMPv6 = 58,
 };
 
+NetworkOrdered<u16> internet_checksum(void const*, size_t);
+inline NetworkOrdered<u16> internet_checksum(void const* ptr, size_t count)
+{
+    u32 checksum = 0;
+    auto* w = (u16 const*)ptr;
+    while (count > 1) {
+        checksum += AK::convert_between_host_and_network_endian(*w++);
+        if (checksum & 0x80000000)
+            checksum = (checksum & 0xffff) | (checksum >> 16);
+        count -= 2;
+    }
+    while (checksum >> 16)
+        checksum = (checksum & 0xffff) + (checksum >> 16);
+    return ~checksum & 0xffff;
+}
+
 }
--- Kernel/Net/IP/IPv4.h
+++ Kernel/Net/IP/IPv4.h
@@ -10,6 +10,7 @@
 #include <AK/Endian.h>
 #include <AK/IPv4Address.h>
 #include <AK/Types.h>
+#include <Kernel/Net/IP/IP.h>
 
 namespace Kernel {
 
@@ -17,8 +18,6 @@ enum class IPv4PacketFlags : u16 {
     DontFragment = 0x4000,
     MoreFragments = 0x2000,
 };
-
-NetworkOrdered<u16> internet_checksum(void const*, size_t);
 
 class [[gnu::packed]] IPv4Packet {
 public:
@@ -99,20 +98,5 @@ private:
 };
 
 static_assert(AssertSize<IPv4Packet, 20>());
-
-inline NetworkOrdered<u16> internet_checksum(void const* ptr, size_t count)
-{
-    u32 checksum = 0;
-    auto* w = (u16 const*)ptr;
-    while (count > 1) {
-        checksum += AK::convert_between_host_and_network_endian(*w++);
-        if (checksum & 0x80000000)
-            checksum = (checksum & 0xffff) | (checksum >> 16);
-        count -= 2;
-    }
-    while (checksum >> 16)
-        checksum = (checksum & 0xffff) + (checksum >> 16);
-    return ~checksum & 0xffff;
-}
 
 }
